6c78fed07c5518bda9599b86d2e6269d267631e0
Hristo Mihaylov <hmih@trichor.local>
2024-04-02 20:15:20 +0200

    count score badly

--- src/main.c
+++ src/main.c
@@ -7,6 +7,9 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 450;
 
+const int PLAYER = 0;
+const int COMPUTER = 1;
+
 const int BAR_WIDTH = 90;
 const int BAR_HEIGHT = 15;
 const int MOVE_SPEED = 10;
@@ -18,7 +21,7 @@ const int FLOOR_BOUND = HOVER_OFFSET + BAR_HEIGHT;
 const int CEIL_BOUND = SCREEN_HEIGHT - FLOOR_BOUND;
 
 const int BALL_RADIUS = 10;
-const int BALL_SPEED = 5;
+const int BALL_SPEED = 85;
 const int BALL_FLOOR_BOUND = FLOOR_BOUND + BALL_RADIUS;
 const int BALL_CEIL_BOUND = CEIL_BOUND - BALL_RADIUS;
 const int BALL_LEFT_SIDE_BOUND = BALL_RADIUS;
@@ -30,15 +33,7 @@ int main(void);
 
 int main(void) {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "pong");
-  const int initial_y = GetRandomValue(-1, 1);
-  const int initial_x = GetRandomValue(-2, 2);
-
-  Vector2 move_computer = {.x = ((float)MOVE_SPEED) / 2, .y = 0};
-  Vector2 move_player = {.x = MOVE_SPEED, .y = 0};
-  Vector2 ball = {BALL_INITIAL_X, BALL_INITIAL_Y};
-  Vector2 direction = {.y = initial_y, .x = initial_x};
-  Vector2 computer = {30, HOVER_OFFSET};
-  Vector2 player = {30, CEIL_BOUND};
+  int points[2] = {0, 0};
 
   SetTargetFPS(60);
   Camera2D camera = {0};
@@ -47,24 +42,46 @@ int main(void) {
   camera.rotation = 0.0f;
   camera.zoom = 1.0f;
 
+  int initial_y;
+  int initial_x;
+  Vector2 move_computer;
+  Vector2 move_player;
+  Vector2 ball;
+  Vector2 direction;
+  Vector2 computer;
+  Vector2 player;
+
+init:
+  initial_y = GetRandomValue(-1, 1);
+  initial_x = GetRandomValue(-2, 2);
+  move_computer = (Vector2){.x = ((float)MOVE_SPEED) / 2, .y = 0};
+  move_player = (Vector2){.x = MOVE_SPEED, .y = 0};
+  ball = (Vector2){BALL_INITIAL_X, BALL_INITIAL_Y};
+  direction = (Vector2){.y = initial_y, .x = initial_x};
+  computer = (Vector2){30, HOVER_OFFSET};
+  player = (Vector2){30, CEIL_BOUND};
+
   while (!WindowShouldClose()) {
 
     ball = Vector2Add(ball, direction);
     ball = (Vector2){
         .x = Clamp(ball.x, BALL_LEFT_SIDE_BOUND, BALL_RIGHT_SIDE_BOUND),
-        .y = Clamp(ball.y, BALL_FLOOR_BOUND, BALL_CEIL_BOUND)};
+        .y = Clamp(ball.y, BALL_FLOOR_BOUND, BALL_CEIL_BOUND),
+    };
 
     if (ball.x < (computer.x + (float)BAR_WIDTH / 2)) {
       computer = Vector2Add(computer, Vector2Negate(move_computer));
     } else if (ball.x > (computer.x - (float)BAR_WIDTH / 2)) {
       computer = Vector2Add(computer, move_computer);
     }
+    computer.x = Clamp(computer.x, BAR_LEFT_BOUND, BAR_RIGHT_BOUND);
 
     if (IsKeyDown(KEY_D)) {
       player = Vector2Add(player, Vector2Negate(move_player));
     } else if (IsKeyDown(KEY_F)) {
       player = Vector2Add(player, move_player);
     }
+    player.x = Clamp(player.x, BAR_LEFT_BOUND, BAR_RIGHT_BOUND);
 
     if (ball.y == BALL_FLOOR_BOUND) {
       if (computer.x <= ball.x && (computer.x + BAR_WIDTH) >= ball.x) {
@@ -72,6 +89,9 @@ int main(void) {
         float a = atan2f(ball.y, ball.x);
         printf("collision angle: %.3f\n", a);
         direction = Vector2Rotate(direction, a);
+      } else {
+        points[PLAYER] += 1;
+        goto init;
       }
     }
 
@@ -81,6 +101,9 @@ int main(void) {
         float a = atan2f(ball.y, ball.x);
         printf("collision angle: %.3f\n", a);
         direction = Vector2Rotate(direction, a);
+      } else {
+        points[COMPUTER] += 1;
+        goto init;
       }
     }
 
@@ -95,10 +118,19 @@ int main(void) {
     ClearBackground(RAYWHITE);
     BeginMode2D(camera);
 
+    char score_computer[2];
+    snprintf(score_computer, 2, "%d", points[COMPUTER]);
+    DrawText(score_computer, 30, FLOOR_BOUND + 30, 20, BLACK);
+
+    char score_player[2];
+    snprintf(score_player, 2, "%d", points[PLAYER]);
+    DrawText(score_player, 30, CEIL_BOUND - 30, 20, BLACK);
+
     Rectangle rect_player = {player.x, player.y, BAR_WIDTH, BAR_HEIGHT};
     DrawRectangleRec(rect_player, RED);
     Rectangle rect_computer = {computer.x, computer.y, BAR_WIDTH, BAR_HEIGHT};
     DrawRectangleRec(rect_computer, BLUE);
+
     DrawCircle(ball.x, ball.y, BALL_RADIUS, BLACK);
 
     EndMode2D();
